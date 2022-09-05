4b29f01aa21eefc149bea28e0ace5404fda1e050
henriquecfaguiar <henriquecfaguiar@gmail.com>
2022-09-05 12:10:22 -0300

    Add regex match and css styles

---
+++ validation.js
@@ -0,0 +1,27 @@
+// let reg = /[a-z]/ig;
+// let reg2 = new RegExp(/[a-z]/, "gi");
+const inputs = document.querySelectorAll("input");
+const patterns = {
+  telephone: /^\d{11}$/,
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^[\w@-]{8,20}$/,
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,5})(\.[a-z]{2,5})?$/
+};
+
+function validate(field, regex) {
+  if(regex.test(field.value)) {
+    field.className = "valid";
+  } else if (field.value === "") {
+    field.classList.remove("invalid");
+    field.classList.remove("valid");
+  } else {
+    field.className = "invalid";
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (e) => {
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
